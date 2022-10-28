575ff5f72eb603bf4e6f4e33cc4606c1046da0c4
Abhijith T R <2020csb1062@iitrpr.ac.in>
2022-10-28 12:09:22 +0530

    Added Release Functionality

--- project.sql
+++ project.sql
@@ -3,17 +3,10 @@ CREATE DATABASE train_system;
 \c train_system;
 
 CREATE TABLE trains (
-  train_number VARCHAR(5) PRIMARY KEY,
-  train_name VARCHAR(50)
+  train_number VARCHAR(5) PRIMARY KEY
 );
 
 -- Trigger to create a new table for each train that is inserted into the trains table 
-
-CREATE TRIGGER insert_into_trains_table
-  BEFORE INSERT
-  ON trains
-  FOR EACH ROW
-  EXECUTE PROCEDURE create_train_table();
 
 CREATE OR REPLACE FUNCTION create_train_table()
 RETURNS TRIGGER AS $$
@@ -22,7 +15,7 @@ RETURNS TRIGGER AS $$
   BEGIN
     EXECUTE format('
       CREATE TABLE if not exists %I (
-        dep_date date,
+        dep_date date PRIMARY KEY,
         ac_seats_total integer,
         sleeper_seats_total integer,
         ac_seats_left integer,
@@ -30,19 +23,31 @@ RETURNS TRIGGER AS $$
       )
     ', NEW.train_number);
 
-    
+    EXECUTE format('
+        CREATE TRIGGER insert_into_specific_train
+        BEFORE INSERT 
+        ON "' || NEW.train_number ||'"
+        FOR EACH ROW
+        EXECUTE PROCEDURE create_train_instance_table();
+    ');
 
     return NEW;
   END
 $$ LANGUAGE plpgsql;
+ 
+CREATE TRIGGER insert_into_trains_table
+  BEFORE INSERT
+  ON trains
+  FOR EACH ROW
+  EXECUTE PROCEDURE create_train_table();
 
 -- Trigger to create a new table for each instance of a train on a particular date
 
-CREATE TRIGGER insert_into_specific_train
-  BEFORE INSERT 
-  ON %I
-  FOR EACH ROW
-  EXECUTE PROCEDURE create_train_instance_table();
+-- CREATE TRIGGER insert_into_specific_train
+--   BEFORE INSERT 
+--   ON || 
+--   FOR EACH ROW
+--   EXECUTE PROCEDURE create_train_instance_table(train_number VARCHAR(5));
 
 CREATE OR REPLACE FUNCTION create_train_instance_table()
 RETURNS TRIGGER AS $$
@@ -52,11 +57,33 @@ RETURNS TRIGGER AS $$
     EXECUTE FORMAT('
       CREATE TABLE %I (
         pnr VARCHAR(16),
+        seat_number VARCHAR(10),
         passenger_name VARCHAR(500), 
-        number_of_seats INTEGER,
-        starting_seat VARCHAR(6)
+        age INTEGER,
+        gender VARCHAR(2)
       )
-    ', )
+    ', TG_TABLE_NAME::text || '-' || NEW.dep_date);
+
     return NEW;
+  END
+$$ LANGUAGE plpgsql;
+
+-- Function to insert trains into system
+
+CREATE OR REPLACE PROCEDURE release_train(
+  train_number VARCHAR(5), 
+  dep_date DATE, 
+  ac_coaches integer, 
+  sl_coaches integer) AS
+$$
+  DECLARE
+
+  BEGIN
+    EXECUTE format('
+      INSERT INTO %I
+      VALUES(%L, %L, %L, %L, %L);
+    ', train_number, dep_date, ac_coaches * 18, sl_coaches * 24, ac_coaches * 18, sl_coaches*24);
+  EXCEPTION
+    RAISE EXCEPTION 'Train has already been released on this date';
   END
 $$ LANGUAGE plpgsql;
