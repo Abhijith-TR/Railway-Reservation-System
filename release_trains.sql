65bce25b20460c9078cbf6da61337482cd57c798
prathamkundan <2020csb1114@iitrpr.ac.in>
2022-11-01 16:35:22 +0530

    changed file structure

---
+++ release_trains.sql
@@ -0,0 +1,26 @@
+-- Function to insert trains into system
+
+CREATE OR REPLACE PROCEDURE release_train(
+  train_number VARCHAR(5), 
+  dep_date DATE, 
+  ac_coaches integer, 
+  sl_coaches integer) AS
+$$
+  DECLARE
+
+  BEGIN
+
+    EXECUTE format('
+      INSERT INTO %I
+      VALUES(%L, %L, %L, %L, %L);
+    ', train_number, dep_date, ac_coaches * 18, sl_coaches * 24, ac_coaches * 18, sl_coaches*24);
+
+
+    EXCEPTION 
+	    WHEN undefined_table THEN 
+	      RAISE EXCEPTION 'Train not found';
+	    WHEN unique_violation THEN 
+	      RAISE EXCEPTION 'Train already in booking system';
+
+  END
+$$ LANGUAGE plpgsql;
---
+++ trains.sql
@@ -0,0 +1,73 @@
+-- Table to store all trains by train number
+CREATE TABLE trains (
+  train_number VARCHAR(5) PRIMARY KEY
+);
+
+-- Trigger to create a new table for each train that is inserted into the trains table 
+
+CREATE OR REPLACE PROCEDURE add_train(train_number VARCHAR(5))
+AS $$
+  DECLARE
+  BEGIN
+    EXECUTE format(
+      '
+        INSERT INTO trains values (%L);
+      ', train_number
+    );
+  END
+$$ LANGUAGE plpgsql;
+
+-- Trigger to create a new table for each instance of a train on a particular date
+
+
+CREATE OR REPLACE FUNCTION create_train_instance_table()
+RETURNS TRIGGER AS $$
+  DECLARE
+
+  BEGIN
+    EXECUTE FORMAT('
+      CREATE TABLE %I (
+        pnr VARCHAR(32),
+        seat_number VARCHAR(10),
+        passenger_name VARCHAR(500), 
+        age INTEGER,
+        gender VARCHAR(2)
+      )
+    ', TG_TABLE_NAME::text || '-' || NEW.dep_date);
+
+    return NEW;
+  END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION create_train_table()
+RETURNS TRIGGER AS $$
+  DECLARE
+
+  BEGIN
+    EXECUTE format('
+      CREATE TABLE if not exists %I (
+        dep_date date PRIMARY KEY,
+        ac_seats_total INTEGER,
+        sl_seats_total INTEGER,
+        ac_seats_left INTEGER,
+        sl_seats_left INTEGER
+      )
+    ', NEW.train_number);
+
+    EXECUTE format('
+        CREATE TRIGGER insert_into_specific_train
+        BEFORE INSERT 
+        ON "' || NEW.train_number ||'"
+        FOR EACH ROW
+        EXECUTE PROCEDURE create_train_instance_table();
+    ');
+
+    return NEW;
+  END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insert_into_trains_table
+  BEFORE INSERT
+  ON trains
+  FOR EACH ROW
+  EXECUTE PROCEDURE create_train_table();
